1b0b9270ca34eab2d17aed83c7897f7f09df0962
Raphael Michel <michel@rami.io>
2022-12-17 16:57:22 +0100

    Stop using deprecated Django APIs

---
+++ pretix_capacity_reports/apps.py
@@ -0,0 +1,26 @@
+from django.utils.translation import gettext_lazy
+from . import __version__
+
+try:
+    from pretix.base.plugins import PluginConfig
+except ImportError:
+    raise RuntimeError("Please use pretix 2.7 or above to run this plugin!")
+
+
+class PluginApp(PluginConfig):
+    name = "pretix_capacity_reports"
+    verbose_name = "Capacity reporting"
+
+    class PretixPluginMeta:
+        name = gettext_lazy("Capacity reporting")
+        author = "pretix team"
+        description = gettext_lazy("Capacity and utilization reports")
+        visible = True
+        version = __version__
+        category = "FORMAT"
+        compatibility = "pretix>=3.14.0"
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
